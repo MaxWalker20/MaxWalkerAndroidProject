a5e06d9f22cf4e3b6b34b768aefcd8ab1689fc77
Max Walker <maxrwalker@btinternet.com>
2022-04-14 09:33:08 +0100

    api URL mid change

--- app/src/main/java/com/example/googlemapsactivity/MapsActivity.java
+++ app/src/main/java/com/example/googlemapsactivity/MapsActivity.java
@@ -14,19 +14,21 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.net.PlacesClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.net.URL;
+
 
 import okhttp3.OkHttpClient;
 import okhttp3.Callback;
 import okhttp3.Request;
 import okhttp3.Call;
 import okhttp3.Response;
+import okhttp3.HttpUrl;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -37,8 +39,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Places.initialize(getApplicationContext(), "AIzaSyB9pAD-HirbHeLIrLDiNBOg2QgPOJBMg54");
-        PlacesClient placesClient = Places.createClient(this);
+
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -70,10 +71,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void buttonPush(View view) {
+
+        URL apiRequestUrl = new HttpUrl.Builder()
+                .scheme("https")
+                .host("maps.googleapis.com")
+                .port(4567)
+                .addPathSegments("foldername/1234")
+                .addQueryParameter("abc", "xyz")
+                .build().url();
+
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyB9pAD-HirbHeLIrLDiNBOg2QgPOJBMg54")
+                .url(apiRequestUrl)
                 .method("GET", null)
                 .build();
 
@@ -102,6 +112,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void updateTextMessage(){
         System.out.println("Button successfully pressed");
         final TextView changingText = (TextView) findViewById(R.id.text_to_change);
+
+
+        /* client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                final String myResponse = response.body().string();
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            JSONObject json = new JSONObject(myResponse);
+                            txtString.setText(json.getJSONObject("data").getString("first_name")+ " "+json.getJSONObject("data").getString("last_name"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+         */
         changingText.setText("Hello");
     }
 }
